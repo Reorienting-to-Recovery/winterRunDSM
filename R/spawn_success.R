cab9fa8a40c7e09565846d2822b89d645be62ff1
liz-fw <lstebbins@flowwest.com>
2024-03-20 13:51:06 -0700

    migrates fallRunDSM logic to spawn_success.R

--- R/spawn_success.R
+++ R/spawn_success.R
@@ -52,24 +52,25 @@ stochastic){
     dplyr::pull(fecundity)
   
   # calculate natural fry
-  natural_fry <- rowSums(sweep(total_nat_spawn * (1 - prob_scour), 2, fecundity_natural, "*") * egg_to_fry_survival)
+  natural_fry <- suppressWarnings(rowSums(sweep(total_nat_spawn * (1 - prob_scour), 2, fecundity_natural, "*") * egg_to_fry_survival))
   
   # nat_spawn_with_ages[1] <- total_nat_spawn * natural_age_distribution
   total_hatch_spawn <- dplyr::tibble(watershed = fallRunDSM::watershed_labels, spawners = round(spawners * (1 - proportion_natural))) |>
     dplyr::left_join(hatchery_age_distribution, by = c("watershed" = "watershed")) |>
     dplyr::mutate(age_2_spawners = round(spawners * prop_2),
                   age_3_spawners = round(spawners * prop_3),
-                  age_4_spawners = round(spawners * prop_4)) |>
-    dplyr::select(-c(prop_2, prop_3, prop_4, spawners, watershed)) |>
+                  age_4_spawners = round(spawners * prop_4),
+                  age_5_spawners = round(spawners * prop_5)) |>
+    dplyr::select(-c(prop_2, prop_3, prop_4, prop_5, spawners, watershed)) |>
     as.matrix()
   
-  dimnames(total_hatch_spawn) <- list(c(fallRunDSM::watershed_labels), c("2", "3", "4"))
+  dimnames(total_hatch_spawn) <- list(c(fallRunDSM::watershed_labels), c("2", "3", "4", "5"))
   fecundity_hatch <- fecundity_lookup |>
     dplyr::filter(origin == "Hatchery") |>
     dplyr::pull(fecundity)
   
   # calculate hatchery fry
-  hatchery_fry <- rowSums(sweep(total_hatch_spawn * (1 - prob_scour), 2, fecundity_hatch, "*") * egg_to_fry_survival)
+  hatchery_fry <- suppressWarnings(rowSums(sweep(total_hatch_spawn * (1 - prob_scour), 2, fecundity_hatch, "*") * egg_to_fry_survival))
   fry <- natural_fry + hatchery_fry
   fry <- if(stochastic) {
     pmax(round(rnorm(31, fry, (sqrt(fry) / 2))), 0)
